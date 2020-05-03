3af7a22e5deb0831e44274a17b0dbd821d898932
Tanmay Pachpande <ptanmay143@gmail.com>
2020-05-03 18:52:01 +0530

    Update dumb-tic-tac-toe.py
    Fix issue where if no party won the game to the last step, it would result in a program error.

--- dumb-tic-tac-toe.py
+++ dumb-tic-tac-toe.py
@@ -3,14 +3,6 @@ import os
 from colorama import init
 from termcolor import colored
 
-if __name__ == "__main__":
-    main()
-
-
-def main():
-    init()
-    Welcome()
-
 
 def Welcome():
     os.system("cls" if os.name == "nt" else "clear")
@@ -19,9 +11,7 @@ def Welcome():
     )
     os.system("cls" if os.name == "nt" else "clear")
     if play == "p":
-        availableChoices = []
-        plays = []
-        PlayGame(availableChoices, plays)
+        PlayGame()
     if play == "q":
         SystemExit
     if not ((play == "p") or (play == "q")):
@@ -29,30 +19,54 @@ def Welcome():
         Welcome()
 
 
-def GameBoard(availableChoices, plays):
+def PlayGame():
+    availableChoices = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    plays = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    gameStatus = -1
+    GameBoard(plays)
+    while gameStatus == -1:
+        availableChoices, plays = TurnUser(availableChoices, plays)
+        GameBoard(plays)
+        gameStatus = CheckWin(plays)
+        if gameStatus != -1:
+            break
+        availableChoices, plays = TurnComputer(availableChoices, plays)
+        GameBoard(plays)
+        gameStatus = CheckWin(plays)
+        if gameStatus != -1:
+            break
+    if gameStatus == 1:
+        print("\nCongratulations! You have won the game. Thank you for playing.")
+        SystemExit
+    if gameStatus == 0:
+        print("\nOops! You have lost the game. Thank you for playing.")
+        SystemExit
+
+
+def GameBoard(plays):
     os.system("cls" if os.name == "nt" else "clear")
     print("\n")
-    GameBoardColor(0)
+    GameBoardColor(plays, 0)
     print("|", end=" ")
-    GameBoardColor(1)
+    GameBoardColor(plays, 1)
     print("|", end=" ")
-    GameBoardColor(2)
+    GameBoardColor(plays, 2)
     print("\n")
-    GameBoardColor(3)
+    GameBoardColor(plays, 3)
     print("|", end=" ")
-    GameBoardColor(4)
+    GameBoardColor(plays, 4)
     print("|", end=" ")
-    GameBoardColor(5)
+    GameBoardColor(plays, 5)
     print("\n")
-    GameBoardColor(6)
+    GameBoardColor(plays, 6)
     print("|", end=" ")
-    GameBoardColor(7)
+    GameBoardColor(plays, 7)
     print("|", end=" ")
-    GameBoardColor(8)
+    GameBoardColor(plays, 8)
     print("\n")
 
 
-def GameBoardColor(availableChoices, plays, choice):
+def GameBoardColor(plays, choice):
     if plays[choice] == "X":
         print(colored(plays[choice], "green"), end=" ")
     elif plays[choice] == "O":
@@ -61,47 +75,7 @@ def GameBoardColor(availableChoices, plays, choice):
         print(plays[choice], end=" ")
 
 
-def PlayGame(availableChoices, plays):
-    availableChoices = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    plays = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    gameStatus = -1
-    GameBoard(plays)
-    while gameStatus == -1:
-        TurnUser()
-        GameBoard(plays)
-        gameStatus = CheckWin()
-        TurnComputer()
-        GameBoard(plays)
-        gameStatus = CheckWin()
-    if gameStatus == 1:
-        print("\nCongratulations! You have won the game. Thank you for playing")
-        SystemExit
-    if gameStatus == 0:
-        print("\nOops! You have lost the game. Thank you for playing")
-        SystemExit
-
-
-def TurnUser(availableChoices, plays):
-    choice = int(input("\nEnter the position where you want to play this round : "))
-    if choice < 9:
-        if choice in availableChoices:
-            plays[choice] = "X"
-            availableChoices.remove(choice)
-        else:
-            print("The position is already filled, try again!")
-            TurnUser()
-    else:
-        print("Invalid position, try again!")
-        TurnUser()
-
-
-def TurnComputer(availableChoices, plays):
-    choice = random.choice(availableChoices)
-    plays[choice] = "O"
-    availableChoices.remove(choice)
-
-
-def CheckWin(availableChoices, plays):
+def CheckWin(plays):
     if plays[0] == plays[1] and plays[1] == plays[2] and plays[2] == "X":
         return 1
     if plays[3] == plays[4] and plays[4] == plays[5] and plays[5] == "X":
@@ -135,3 +109,30 @@ def CheckWin(availableChoices, plays):
     if plays[2] == plays[4] and plays[4] == plays[6] and plays[6] == "O":
         return 0
     return -1
+
+
+def TurnUser(availableChoices, plays):
+    choice = int(input("\nEnter the position where you want to play this round : "))
+    if choice < 9:
+        if choice in availableChoices:
+            plays[choice] = "X"
+            availableChoices.remove(choice)
+        else:
+            print("The position is already filled, try again!")
+            TurnUser()
+    else:
+        print("Invalid position, try again!")
+        TurnUser()
+    return availableChoices, plays
+
+
+def TurnComputer(availableChoices, plays):
+    choice = random.choice(availableChoices)
+    plays[choice] = "O"
+    availableChoices.remove(choice)
+    return availableChoices, plays
+
+
+if __name__ == "__main__":
+    init()
+    Welcome()
